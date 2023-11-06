358c30b8423236d40006b6c83f848cfcd67500f5
CatAvadani <catalinaavadani@gmail.com>
2023-11-06 19:22:56 +0100

    some refactoring

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ function main() {
   displayScene();
 }
 
-function displayScene() {
-  // select the elements
-  const sceneTitle = document.getElementById("title");
-  const sceneDescription = document.getElementById("description");
-  const buttons = document.querySelector(".buttons");
-  const btn1 = document.getElementById("btn-1");
-  const btn2 = document.getElementById("btn-2");
-  const instruction = document.getElementById("instruction");
-  const backgroundScene = document.getElementById("background-img");
-  const hiddenMap = document.getElementById("hidden-map");
-  const invertoryCollection = document.querySelector(".inventory-collection");
-  const backgroundVideo = document.getElementById("background-video");
+// select the elements
+const sceneTitle = document.getElementById("title");
+const sceneDescription = document.getElementById("description");
+const buttons = document.querySelector(".buttons");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const instruction = document.getElementById("instruction");
+const backgroundScene = document.getElementById("background-img");
+const hiddenMap = document.getElementById("hidden-map");
+const invertoryCollection = document.querySelector(".inventory-collection");
+const backgroundVideo = document.getElementById("background-video");
 
+function displayScene() {
   // select the current scene
   const currentScene = scenes[currentSceneIndex];
   sceneTitle.textContent = currentScene.title;
@@ -26,35 +26,37 @@ function displayScene() {
   btn1.textContent = currentScene.button1.title;
   btn2.textContent = currentScene.button2.title;
 
-  btn1.classList.add("hidden");
-  btn2.classList.add("hidden");
+  hideButtons();
   backgroundScene.classList.add("hidden");
 
   hiddenMap.addEventListener("click", () => {
     displayNextScene(currentScene.button1.nextScene);
-    // backgroundScene.setAttribute("src", "images/hidden-map.png");
-    btn1.classList.remove("hidden");
-    btn2.classList.remove("hidden");
     hiddenMap.classList.add("hidden");
     backgroundVideo.classList.add("hidden");
     backgroundScene.classList.remove("hidden");
+    showButtons();
   });
 
   btn1.addEventListener("click", () => {
     displayNextScene(currentScene.button1.nextScene);
-    btn1.classList.remove("hidden");
-    btn2.classList.remove("hidden");
     backgroundScene.classList.remove("hidden");
+    showButtons();
   });
   btn2.addEventListener("click", () => {
     displayNextScene(currentScene.button2.nextScene);
-    btn1.classList.remove("hidden");
-    btn2.classList.remove("hidden");
     backgroundScene.classList.remove("hidden");
+    showButtons();
   });
 }
 
-function hideButtons() {}
+function hideButtons() {
+  btn1.classList.add("hidden");
+  btn2.classList.add("hidden");
+}
+function showButtons() {
+  btn1.classList.remove("hidden");
+  btn2.classList.remove("hidden");
+}
 
 function displayNextScene(index) {
   currentSceneIndex = index;
